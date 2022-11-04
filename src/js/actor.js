65d8ac5c7628176ac5eec31cfb1508933793c12d
Adhamjon Erkinov <adhamjonerkinov65@gmail.com>
2022-11-04 19:49:31 +0500

    nimadir

--- src/js/actor.js
+++ src/js/actor.js
@@ -33,8 +33,8 @@ export function displayActor(actor = []) {
                  ${biography}
               </p>
           </div>
-          <div class="crew__films"></div>
          
+          <div class="cast__films"></div>
   
       </div>
        </div>
@@ -46,35 +46,62 @@ export function displayCastActor(cast) {
     let result = "";
     const authorMenuNode = document.querySelector(".cast__films");
     cast.forEach((person) => {
-      const { id, poster_path, title, character } = person;
-      const img = poster_path
-        ? configs.baseImgURL + poster_path
+      const { id, backdrop_path, title, release_date } = person;
+      const img = backdrop_path
+        ? configs.baseImgURL + backdrop_path
         : configs.defaultImg + "500";
       configs.baseImgURL;
       const cardImg = configs.cardImg;
-      result += `
-      
-      
-      <div class="col actors__cols">
-       <div class="card">
-        <div class="card__head">
-          <div class="card__img_top" data-id="${id}">
-            <img width="100%" src="${img}" alt="Movies__Pecture">
-          </div>
-        </div>
-          
-        <div class="card__body">
-          <div class="card__title">
-            <p> ${title}</p>
-          </div>
-          <div class="card__date">
-            <p>${character}</p>
-          </div>
-          </div>
-        </div>
-        
+      result +=`<div class="col"> <article class="card actor__cards" data-id="${id}">
+      <div class="card__header dropdown">
+        <svg
+          id="glyphicons-basic"
+          class="card__menu__btn dropdown__btn"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+        >
+          <path
+            id="circle-more"
+            d="M16,4A12,12,0,1,0,28,16,12.01312,12.01312,0,0,0,16,4ZM10,18a2,2,0,1,1,2-2A2.00006,2.00006,0,0,1,10,18Zm6,0a2,2,0,1,1,2-2A2.00006,2.00006,0,0,1,16,18Zm6,0a2,2,0,1,1,2-2A2.00006,2.00006,0,0,1,22,18Z"
+            fill="#ffffff80"
+          ></path>
+        </svg>
+        <ul class="card__menu dropdown__content">
+          <li class="dropdown__item">
+            <a href="dropdown__link">
+              <i class="fas fa-heart"></i> favourite
+            </a>
+          </li>
+          <li class="dropdown__item">
+            <a href="dropdown__link">
+              <i class="fas fa-heart"></i> favourite
+            </a>
+          </li>
+          <li class="dropdown__item">
+            <a href="dropdown__link">
+              <i class="fas fa-heart"></i> favourite
+            </a>
+          </li>
+          <li class="dropdown__item">
+            <a href="dropdown__link">
+              <i class="fas fa-heart"></i> favourite
+            </a>
+          </li>
+        </ul>
       </div>
-      `;
+      <div class="card__img--wrapper">
+        <img
+          class="card__img"
+          src="${img}"
+          alt="${title}"
+        />
+      </div>
+      <div class="card__body card__percentage">
+        <div class="percentage">${81}</div>
+        <h4 class="card__title">${title}</h4>
+        <p class="card__text">${release_date}</p>
+      </div>
+    </article></div>`;
     });
     authorMenuNode.innerHTML = result;
   }
@@ -83,31 +110,16 @@ export function displayCastActor(cast) {
     let result = "";
     const authorMenuNode = document.querySelector(".crew__films");
     crew.forEach((person) => {
-      const { id, poster_path, title } = person;
-      const img = poster_path
-        ? configs.baseImgURL + poster_path
-        : configs.defaultImg + "500";
+      const { title } = person;
       result += `
       
-      <div class="col details__cols">
-       <div class="card">
-        <div class="card__head">
-          <div class="card__img_top" data-id="${id}">
-            <img width="100%" src="${img}" alt="Movies__Pecture">
-          </div>
-        </div>
-          
-        <div class="card__body">
-          <div class="card__title">
-            <p> ${title}</p>
-          </div>
-          <div class="card__date">
-          
-          </div>
-          </div>
-        </div>
-        
-      </div>
+   <div class="acting">
+     <p></p>
+     <span></span>
+     
+     <div class="acting__title"> ${title}</div>
+
+   </div>
       `;
     });
     authorMenuNode.innerHTML = result;
